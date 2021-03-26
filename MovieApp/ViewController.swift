7f89b3a4a9ef02830dea35a1f6fb095cf33db4e5
Matheus Golke Cardoso <matheusgoolke@gmail.com>
2021-03-26 14:20:13 -0300

    Testando chamada da API + instalação do alamofire

--- MovieApp/ViewController.swift
+++ MovieApp/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  MovieApp
+//  testingAPIMovie
 //
 //  Created by Matheus Golke Cardoso on 26/03/21.
 //  Copyright © 2021 Matheus. All rights reserved.
@@ -8,13 +8,115 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getData()
     }
-
-
+    
+    func getData(){
+        
+        let urlString = "https://api.themoviedb.org/3/trending/all/week?api_key=92f48df587a0b620ed4b4a7ecc9b9159&language=pt-BR"
+        
+        guard let url = URL(string: urlString) else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            //print(response)
+            guard let data = data, error == nil else {return}
+            
+            var resultado: Welcome?
+            
+            do{
+                
+                resultado = try JSONDecoder().decode(Welcome.self, from: data)
+                
+            }catch{
+                print(error.localizedDescription)
+            }
+            
+            guard let resultadoFinal = resultado else {return}
+            
+            print(resultadoFinal.results.first?.originalTitle)
+        }
+        
+        task.resume()
+    }
+    
+    
 }
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try? newJSONDecoder().decode(Welcome.self, from: jsonData)
+
+import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let page: Int
+    let results: [Result]
+    let totalPages, totalResults: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let adult: Bool?
+    let backdropPath: String
+    let genreIDS: [Int]
+    let id: Int
+    let originalLanguage: OriginalLanguage
+    let originalTitle: String?
+    let overview, posterPath: String
+    let releaseDate, title: String?
+    let video: Bool?
+    let voteAverage: Double
+    let voteCount: Int
+    let popularity: Double
+    let mediaType: MediaType
+    let originCountry: [String]?
+    let firstAirDate, name, originalName: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+        case popularity
+        case mediaType = "media_type"
+        case originCountry = "origin_country"
+        case firstAirDate = "first_air_date"
+        case name
+        case originalName = "original_name"
+    }
+}
+
+enum MediaType: String, Codable {
+    case movie = "movie"
+    case tv = "tv"
+}
+
+enum OriginalLanguage: String, Codable {
+    case en = "en"
+    case es = "es"
+    case ja = "ja"
+}
+
+
 
