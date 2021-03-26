7d31fec7a31e0b8c0cb84bd7d9cb8e86bf00fc12
Matheus Golke Cardoso <matheusgoolke@gmail.com>
2021-03-26 14:07:32 -0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Matheus Golke Cardoso on 26/03/21.
+//  Copyright © 2021 Matheus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
