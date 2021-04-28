38daa7ad768220137103a00c9c2ec46a38fea3b8
Matheus Golke Cardoso <matheusgoolke@gmail.com>
2021-04-28 17:54:29 -0300

    Implementação teste FBSnapshotTestCase

--- MovieAppTests/MovieAppSnapShot.swift
+++ MovieAppTests/MovieAppSnapShot.swift
@@ -7,30 +7,33 @@
 //
 //
 //
-//import FBSnapshotTestCase
-//@testable import MovieApp
-//
-//class MovieAppSnapShot: FBSnapshotTestCase {
-//
-//    var sut: ViewController!
-//
-//    override func setUp() {
-//        super.setUp()
-//        sut = ViewController()
-//        _ = sut.view
-//        self.recordMode = true
-//    }
-//
-//    override func tearDown() {
-//        super.tearDown()
-//        sut = nil
-//    }
-//
-//    func testExample() {
-//        FBSnapshotVerifyView(sut.view)
-//        //FBSnapshotVerifyViewController(sut)
-//
-//
-//    }
-//
-//}
+import FBSnapshotTestCase
+@testable import MovieApp
+
+class MovieAppSnapShot: FBSnapshotTestCase {
+
+    var sut: ViewController!
+
+    override func setUp() {
+        super.setUp()
+        //sut = ViewController()
+        //_ = sut.view
+        sut = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "controller") as! ViewController
+        //self.recordMode = true
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        //sut = nil
+    }
+
+    func testExample() {
+        //FBSnapshotVerifyView(sut.view)
+        //FBSnapshotVerifyViewController(sut)
+        
+        sut.getData()
+        sleep(10)
+        FBSnapshotVerifyView(sut.view)
+    }
+
+}
